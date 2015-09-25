c48bd6197fb865dba10452a3181c5c0c3b03ab9e
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-09-25 14:40:45 +0100

    Updated run.sh to always run java from the script directory

--- ciao-parent-support/src/main/resources/run.sh
+++ ciao-parent-support/src/main/resources/run.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+# Change working directory to CIP directory
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
+
 java -cp "config:${project.artifactId}-${project.version}.jar" -javaagent:lib/jolokia-jvm-${jolokia.version}-agent.jar=config=config/jolokia.properties ${ciao.main}
